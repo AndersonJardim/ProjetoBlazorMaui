e5383c4fd8d3c22f1fc90f495b82cefb03d08bec
Anderson Jardim <andersonjardim@gmail.com>
2024-01-11 01:46:05 -0300

    Primeiro commit com configurações

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProjetoBlazorMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Título do README.md 
+ 
+Este é um texto de exemplo. 
+Mais linhas serão adicionar conforme necessário. 
+ 
+Salvo com sucesso! 
